9afbafd36dd6c947b8e0674cbb56223fd1606548
Greg Seaton <greg.seaton@sierralogic.com>
2019-03-21 11:46:51 -0700

    updated project link

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject templar "0.1.2"
   :description "Templar is a logical grouping of functions into namespaces library for dependency inversion."
-  :url "http://sierralogic.com/post/templar"
+  :url "http://sierralogic.github.io/templar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
